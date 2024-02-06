a4eec3b4cd474c005569604b185e4f1621762066
hasaigal <hasaigal@gmail.com>
2024-02-06 19:46:57 +0500

    n

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import FormAddPost from "./FormAddPost";
+import Posts from "./Posts";
+
+function Main() {
+  // const {posts, onAddPost } = usePosts();
+  return (
+    <main>
+      <FormAddPost />
+      <Posts />
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/components/SearchPosts.jsx
@@ -0,0 +1,14 @@
+import { usePosts } from "../context/PostProvider";
+
+function SearchPosts() {
+  const { searchQuery, setSearchQuery } = usePosts();
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
+
+export default SearchPosts;
